ee1cb5b7460830968e44a78863076181380cb597
Alexandre Roussel <roussela27@gmail.com>
2019-11-19 16:01:21 +0100

    Fonction generation message

--- Hamming/CodeHamming.py
+++ Hamming/CodeHamming.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 
-def messagealea():
+def messageAlea():
     return np.random.randint(2, size=(4,1))
 
-G=np.matrix([[1,1,0,1],[1,0,1,1],[1,0,0,0],[0,1,1,1],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
-print("G=",G)
-C=messagealea()
-print("C=",C)
-M=np.mod(np.dot(G,C),2)
-print("M=",M)
+def genererMessage():
+    G=np.matrix([[1,1,0,1],[1,0,1,1],[1,0,0,0],[0,1,1,1],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+    C=messageAlea()
+    M=np.mod(np.dot(G,C),2)
+    print("M=",M)
+    return M
+
+genererMessage()
