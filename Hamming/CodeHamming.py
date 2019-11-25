258de5634dedd0df1950d36e21d460e5e1470f3d
Alexandre Roussel <roussela27@gmail.com>
2019-11-25 09:13:58 +0100

    fusion des fonctions + decodage message fin

--- Hamming/CodeHamming.py
+++ Hamming/CodeHamming.py
@@ -21,22 +21,29 @@ def bitErreur(message):
 
 def bitErreur(message):
     matriceControle = np.matrix([[0, 0, 0, 1, 1, 1, 1], [0, 1, 1, 0, 0, 1, 1], [1, 0, 1, 0, 1, 0, 1]])
-    R=np.mod(np.dot(matriceControle,message),2);
+    R=np.mod(np.dot(matriceControle,message),2)
     for i in range (matriceControle.shape[1]) :
         if np.allclose(R,matriceControle[:,i]):
-            print("erreur sur le bit : ",i+1)
-            return corrigerErreur(message,i)
+            return i
 
 def corrigerErreur(message,i):
     message[i]=(message[i]+1)%2
     return message
 
+def decodeMessage(message):
+    D = np.matrix([[0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 1]])
+    return np.mod(np.dot(D, message), 2)
+
 
 C=messageAlea()
 print("Donnée a transmettre = ",C)
 M=genererMessage(C)
-print("M = ",M)
+print("Message a envoyer = ",M)
 K=genererErreur(M)
-print("K = ",K)
-L=bitErreur(K)
-print("L = ",L)
+print("Message reçu = ",K)
+i=bitErreur(K)
+print("Erreur sur le bit = ",i+1)
+L=corrigerErreur(K,i)
+print("Message corrigé = ",L)
+P=decodeMessage(L)
+print("Message Décodé = ",P)
