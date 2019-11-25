5122fc6b3d4ab89c4504e23d2539446cc696bafc
Alexandre Roussel <roussela27@gmail.com>
2019-11-25 08:44:32 +0100

    affichage Message envoyé

--- Hamming/CodeHamming.py
+++ Hamming/CodeHamming.py
@@ -1,14 +1,28 @@
 import numpy as np
 
-
 def messageAlea():
     return np.random.randint(2, size=(4,1))
 
-def genererMessage():
+def genererMessage(message):
     G=np.matrix([[1,1,0,1],[1,0,1,1],[1,0,0,0],[0,1,1,1],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
-    C=messageAlea()
-    M=np.mod(np.dot(G,C),2)
-    print("M=",M)
+    M=np.mod(np.dot(G,message),2)
     return M
 
-genererMessage()
+def genererErreur(message):
+    b=np.random.randint(len(message))
+    message[b]=(message[b]+1)%2
+    return message
+
+def bitErreur(message):
+    matriceControle = np.matrix([[0, 0, 0, 1, 1, 1, 1], [0, 1, 1, 0, 0, 1, 1], [1, 0, 1, 0, 1, 0, 1]])
+    for i in range (matriceControle.shape[1]) :
+        if np.allclose(message,matriceControle[:,i]):
+            print("erreur sur le bit : ",i+1)
+
+C=messageAlea()
+print("C = ",C)
+M=genererMessage(C)
+print("M = ",M)
+K=genererErreur(M)
+print("K = ",K)
+bitErreur(K)
