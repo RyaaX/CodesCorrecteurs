8876f7043d4bd20e28049bdd73dd562dee89b9f4
21806985 <21806985@etu.unicaen.fr>
2019-11-25 08:23:33 +0100

    avec la matrice de controle

--- Hamming/fonctionControle.py
+++ Hamming/fonctionControle.py
@@ -4,3 +4,5 @@ def bitErreur(message,matriceControle):
     for i in range (matriceControle.shape[1]) :
         if np.allclose(message,matriceControle[:,i]):
             print("erreur sur le bit : ",i+1)
+
+matriceControle=np.matrix([[0,0,0,1,1,1,1],[0,1,1,0,0,1,1],[1,0,1,0,1,0,1]])
