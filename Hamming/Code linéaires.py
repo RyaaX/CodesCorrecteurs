8873c33f3db731a33351fe52ab3000105325b4c8
yodaluc <luc.audabram1@gmail.com>
2019-11-25 17:59:38 +0100

    voila

--- Hamming/Code linéaires.py
+++ Hamming/Code linéaires.py
@@ -1,38 +1,59 @@
 import numpy as np
 from random import *
 
+
 def generateMot(complexite):
     mot = np.array([])
-    for k in range(0,complexite):
-        mot = np.insert(mot,k,randint(0,1))
+    for k in range(0, complexite):
+        mot = np.insert(mot, k, randint(0, 1))
     return mot
 
 
 def generateMatriceAleatoire(ligne, complexite):
     mot = generateMot(complexite)
-    M = np.array(mot)
-    for nb in range(1,ligne):
+    M = np.array([mot])
+    for nb in range(1, ligne):
         bool = True
         while bool:
             mot = generateMot(complexite)
             dedans = False
 
             for l in M:
-                 dedans = dedans or np.equal(l,mot).all()
+                dedans = dedans or np.equal(l, mot).all()
 
             if not dedans:
-                M = np.vstack([M,mot])
+                M = np.vstack([M, mot])
                 bool = False
     return M
 
 
+def verifierGenerateur(matrice, nbBit):
+    init = np.array([])
+    for k in range(0, nbBit):
+        init = np.insert(init, k, 0)
+    for k in range(1, 2 ** nbBit+1):
+        print(init)
+        init = ajoutUnBinaire(init)
+
+
+def ajoutUnBinaire(mot):
+    for k in range(1, mot.size+1):
+        var = mot[-k]
+        if var == 1:
+            mot[-k] = 0
+        else:
+            mot[-k] = 1
+            break
+    return mot
+
+
 def generateMatriceGeneratrice(ligne, complexite):
     MG = "Pas assez de complexite"
-    if(ligne <= complexite**2):
+    if (ligne <= complexite ** 2):
         M = np.identity(ligne)
         G = generateMatriceAleatoire(ligne, complexite)
-        MG = np.concatenate((M,G),1)
+        MG = np.concatenate((M, G), 1)
     return MG
 
-
-print(generateMatriceGeneratrice(4,3))
+verifierGenerateur([], 2)
+# print(generateMatriceGeneratrice(5, 4))
