4e4eab19e27c75f59a6f49dfe7adf0d6274aff47
yodaluc <luc.audabram1@gmail.com>
2019-12-03 14:11:41 +0100

    Update Code linéaires.py

--- Hamming/Code linéaires.py
+++ Hamming/Code linéaires.py
@@ -27,22 +27,45 @@ def generateMatriceAleatoire(ligne, complexite):
     return M
 
 
-def verifierGenerateur(matrice, nbBit):
-    init = np.array([])
-    for k in range(0, nbBit):
-        init = np.insert(init, k, 0)
+def verifierGenerateur(matrice, nbBit, complexite):
+    init = np.array([[0]])
+    listMot = 0
+    minHam = -1
+    for k in range(0, nbBit-1):
+        init = np.vstack([init, 0])
     for k in range(1, 2 ** nbBit+1):
-        print(init)
-        init = ajoutUnBinaire(init)
+        motCode = init * matrice
 
+        if k == 1:
+            listMot = np.array([motCode])
+        else:
+            listMot = np.vstack([listMot, [motCode]])
+            for g in range(1,k-1):
+                distance = calculHamming(motCode, listMot[g], nbBit+complexite)
+                if minHam == -1 or minHam > distance:
+                    minHam = distance
+
+        init = ajoutUnBinaire(init)
+    return minHam
+
+def calculHamming(Mot1, Mot2, nbBit):
+    distance = 0
+    lenght = np.size(Mot1)
+    for n in range(0, round(lenght/nbBit)):
+        mot1 = Mot1[n]
+        mot2 = Mot2[n]
+        for k in range(0, nbBit):
+            if mot1[k] != mot2[k]:
+                distance = distance + 1
+    return distance
 
 def ajoutUnBinaire(mot):
     for k in range(1, mot.size+1):
         var = mot[-k]
-        if var == 1:
-            mot[-k] = 0
+        if var == [1]:
+            mot[-k] = [0]
         else:
-            mot[-k] = 1
+            mot[-k] = [1]
             break
     return mot
 
@@ -53,7 +76,12 @@ def generateMatriceGeneratrice(ligne, complexite):
         M = np.identity(ligne)
         G = generateMatriceAleatoire(ligne, complexite)
         MG = np.concatenate((M, G), 1)
-    return MG
+    return MG   
 
-verifierGenerateur([], 2)
-# print(generateMatriceGeneratrice(5, 4))
+M = generateMatriceGeneratrice(3, 10)
+H = verifierGenerateur(M, 3, 10)
+
+print("Matrice : ")
+print(M)
+print("Distance de hamming minimal : ")
+print(H)
